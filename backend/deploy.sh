cba6d35dedc17f53c09d300e6b73d1f29e31fd80
Topher Hooper <hooper.christopher@gmail.com>
2025-01-17 21:01:57 -0600

    working

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -22,7 +22,15 @@ SERVICE_EXISTS=$(gcloud run services describe chatbot-backend \
   --format="value(status.url)" 2>/dev/null || true)
 
 if [ -n "$SERVICE_EXISTS" ]; then
-  echo "Updating existing service: chatbot-backend"
+  echo "Updating existing backend service"
+  # Configure docker auth
+  gcloud auth configure-docker --quiet || exit 1
+  
+  docker build \
+    -t gcr.io/fluted-citizen-269819/chatbot-backend . || exit 1
+    
+  docker push gcr.io/fluted-citizen-269819/chatbot-backend || exit 1
+  
   # Update existing service
   gcloud run deploy chatbot-backend \
     --image gcr.io/fluted-citizen-269819/chatbot-backend \
--- backend/main.py
+++ backend/main.py
@@ -10,12 +10,11 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        # "http://127.0.0.1:5173",
-        # "http://localhost:5173", 
-        # "https://topherhooper.com",
-        # "https://chatbot-frontend-614936797883.us-central1.run.app",  # Frontend Cloud Run URL
-        # "https://chatbot-backend-614936797883.us-central1.run.app"    # Backend Cloud Run URL
-        "*"
+        "http://127.0.0.1:5173",
+        "http://localhost:5173", 
+        "https://topherhooper.com",
+        "https://chatbot-frontend-614936797883.us-central1.run.app",  # Frontend Cloud Run URL
+        "https://chatbot-backend-614936797883.us-central1.run.app"    # Backend Cloud Run URL
     ],
     allow_credentials=True,
     allow_methods=["*"],
